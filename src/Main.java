aef2c1c270c5f690b5f91b157d097a7d0165bdb0
timmy <vanteremail@gmail.com>
2024-03-20 21:16:50 +0100

    DATA

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import Database._DATA;
+import Model.Guest;
+import Model.User;
+
+import java.util.ArrayList;
+
 
 public class Main {
+
+
      public static void main(String[]any) {
-         _DATA.odczyt_zadan("DATABASE.ser");
-         _DATA.odczyt_uzytkownikow("DATABASE.ser");
-         System.out.println("Hello");
+         ArrayList <User> Data = new ArrayList<User>();
+         /*_DATA.odczyt_zadan("DATABASE.ser");
+         _DATA.odczyt_uzytkownikow("DATABASE.ser");*/
+         Data = _DATA.odczyt_uzytkownikow("DATABASE.ser");
+         System.out.println(Data.get(0));
      }
 }
